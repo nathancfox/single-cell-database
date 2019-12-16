496a0a98a2a27e89abb4110ec0f414465c9dc04f
Nathan Fox <nchristopherfox@gmail.com>
2019-12-16 18:14:01 -0500

    Added a method to download GSE supplementary files
    Added a method: get_series_suppl_files() to geo_access.py
    that scrapes and gunzips all supplementary files associated
    with a GEO Series ID. Also added a helper method for constructing
    the FTP folder URL. This method, get_series_suppl_url(), is
    slightly different than its sister method, get_series_soft_url(),
    because it returns a URL to a folder instead of to a specific
    file. This new method also does not return an exit code like
    get_series_soft_file() because it does not know in advance what
    files it should be expecting.

--- src/geo_access.py
+++ src/geo_access.py
@@ -50,6 +50,29 @@ def get_series_soft_url(gse_id):
                   + '/soft/' + gse_id + '_family.soft.gz')
     return(ftp_url_full)
 
+def get_series_suppl_url(gse_id):
+    """Create FTP URL for GEO Series supplementary files.
+
+    Given a GEO Series ID, constructs an FTP URL to download
+    the associated supplementary files. The URL will point to
+    the folder holding the supplementary files, not to any
+    particular file.
+
+    Args:
+        gse_id: String of the regex form 'GSE[0-9]+'. The GEO Series ID to
+            convert.
+    
+    Returns:
+        String. The full FTP URL to the supplementary files folder
+        associated with the GEO Series indicated by gse_id.
+
+    Raises: None
+    """
+    ftp_url_base = 'ftp://ftp.ncbi.nlm.nih.gov/geo/series/'
+    gse_folder = convert_gse_to_folder(gse_id)
+    ftp_url_full = (ftp_url_base + gse_folder + gse_id + '/suppl/')
+    return(ftp_url_full)
+
 def get_series_soft_file(gse_id, path):
     """Download and unzip a GEO Series SOFT file.
 
@@ -94,6 +117,51 @@ def get_series_soft_file(gse_id, path):
         return(0)
     else:
         return(1)
+
+def get_series_suppl_files(gse_id, path):
+    """Download and unzip a GEO Series's supplementary files.
+
+    Given a path and a GEO Series ID, downloads the associated
+    supplementary files, and gunzips them in the given path. Unlike
+    the similar method, get_series_soft_file(), this method does
+    not return an exit code, since it does not know the files it
+    should be downloading.
+
+    NOTE: Creates and removes a temporary bash script file called:
+        'get_series_suppl_file_temp.sh'
+    in the working directory. This method will fail if there is
+    already a file called that, to avoid overwriting anything.
+
+    Args:
+        gse_id: String of the regex form 'GSE[0-9]+'. The GEO Series ID to
+            convert.
+        path: String containing the path in which to store the
+            GSE supplementary files.
+
+    Returns: None
+    
+    Raises:
+        Error if this method will overwrite a file with its temporary
+        bash script.
+    """
+    ftp_url = get_series_suppl_url(gse_id)
+    bash_curl_script = ('wget -q --no-parent --recursive --level=1 '
+                       f'--no-directories -P {path} \"{ftp_url}\"')
+    bash_gunzip_script = (f'gunzip {path}*')
+    if os.path.exists('get_series_suppl_file_temp.sh'):
+        sys.exit(('File \"get_series_suppl_file_temp.sh\" already exists'
+                  ' and will be overwritten by a temp file created by'
+                  ' this method. Please rename it to something else'
+                  ' to save it.'))
+    with open('get_series_suppl_file_temp.sh', 'w') as f:
+        f.write(bash_curl_script)
+        f.write('\n')
+        f.write(bash_gunzip_script)
+        f.write('\n')
+    os.system('bash get_series_suppl_file_temp.sh')
+    os.remove('get_series_suppl_file_temp.sh')
+    
+
     
 def main():
     pass
