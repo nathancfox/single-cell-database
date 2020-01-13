6743b9795f80083169efe6813abc23343ea94cb2
Nathan Fox <nchristopherfox@gmail.com>
2020-01-13 11:17:04 -0500

    Add pretty print function for list

--- src/general_utils.py
+++ src/general_utils.py
@@ -87,6 +87,98 @@ def gunzip(files, remove = True):
         if remove:
             os.remove(os.path.join(path, filename)) 
     
+def pretty_str_list(list, width = 50, indent = '',
+                    print_str = False): 
+    """Creates pretty string of a list.
+
+    Creates a pretty string of the items in a list within
+    the given width, with the given indent in front of each
+    line. Unlike pprint, this method does not quote or
+    annotate items in any way. This makes it better for
+    printing lists for display, rather than for examination.
+    
+    For example:
+        >>> test = ['apple', 'banana', 'cherry', 'date',
+                   'elderberry', 'fig', 'grapefruit'] 
+        >>> test
+        ['apple', 'banana', 'cherry', 'date', 'elderberry', 'fig', 'grapefruit']
+        >>> print(pprint.pformat(test, width = 30, indent = 4))
+        [    'apple',
+             'banana',
+             'cherry',
+             'date',
+             'elderberry',
+             'fig',
+             'grapefruit']
+        >>> print(pretty_str_list(test, width = 30, indent = '    '))
+            apple, banana, cherry,
+            date, elderberry, fig,
+            grapefruit
+
+    Building a string in a loop via concatenation is bad practice.
+    Any operations that add to the end of the pretty string are
+    done by adding substrings to a list, then joining at the end.
+    This operation takes linear time, rather than quadratic.
+    However, string concatenations that take place on a single
+    item, such as adding the ", " are left as string concatenations
+    for clarity, since they do not involve the main pretty string.
+
+    Args:
+        list: The list to convert into a pretty string. Items are
+            automatically converted to their string representation,
+            so, within reason, the list items do not have to be
+            strings.
+        width: Integer. The maximum character width of each line,
+            including the indent.
+        indent: A string to preprend to each line. For example, to
+            indent the whole output, use '    ' (4 whitespaces).
+        print_str: Boolean. If True, the pretty string will be printed,
+            and None will be returned. If False, the pretty string
+            will be returned and nothing will be printed.
+        
+    Returns:
+        A single string containing appropriate new lines and indents
+        that can be printed. Contains a pretty string version of the
+        list.
+
+    Raises: None
+    """    
+    line_counter = 0 
+    line_width = 0 
+    str_out = []
+    str_out.append(indent)
+    width -= len(indent) 
+    for idx, item in enumerate(list): 
+        item = str(item) 
+        if idx < len(list) - 1: 
+            item += ',' 
+        if len(item) >= width: 
+            if line_counter != 0: 
+                str_out.append('\n' + indent) 
+            str_out.append(item)
+            str_out.append('\n' + indent)
+            line_counter += 2 
+            line_width = 0 
+        else: 
+            # + 1 is for the whitespace after the comma.
+            if len(item) + 1 + line_width > width: 
+                str_out.append('\n' + indent) 
+                str_out.append(item)
+                line_counter += 1 
+                line_width = len(item) 
+            else: 
+                if line_counter == 0 and line_width == 0: 
+                    pass 
+                else: 
+                    item = ' ' + item 
+                str_out.append(item)
+                line_width += len(item) 
+    str_out = ''.join(str_out)
+    if print_str:
+        print(str_out)
+    else:
+        return(str_out)
+
 def main():
     pass
 
