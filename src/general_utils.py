ecd299c1ba376e880302efc3df08e8852d3b6430
Nathan Fox <nchristopherfox@gmail.com>
2020-02-06 15:48:17 -0500

    Add 2 new utility functions and fix bugs
    I added 2 new functions to general_utils.py. The first one is
    similar to get_yes_or_no(). It provides a robust way to get
    a text command from the user on the command line. The second one
    is a special case of pretty_str_list. It uses pretty_str_list()
    to create a left-aligned width-constrained pretty printable
    version of a long string. Creating this resulted in a redesign
    of pretty_str_list(), but the input/output is the same.

--- src/general_utils.py
+++ src/general_utils.py
@@ -15,6 +15,7 @@ import gzip
 import shutil
 import tarfile
 import pandas as pd
+import string
 
 def get_timestamp(mode = 'both', long = True):
     """Generates string timestamp.
@@ -118,7 +119,7 @@ def extract_tar(tar_file, outpath = "", remove = True):
                'filename. Please extract manually!')
     
 def pretty_str_list(list, width = 50, indent = '',
-                    one_per_line = False): 
+                    sep = ', ', one_per_line = False): 
     """Creates pretty string of a list.
 
     Creates a pretty string of the items in a list within
@@ -156,6 +157,7 @@ def pretty_str_list(list, width = 50, indent = '',
             including the indent.
         indent: A string to prepend to each line. For example, to
             indent the whole output, use '    ' (4 whitespaces).
+        sep: A string to use to separate entries on a single line.
         one_per_line: Boolean. If True, all elements will be printed
             on their own lines, without commas, regardless of
             element length.
@@ -180,10 +182,24 @@ def pretty_str_list(list, width = 50, indent = '',
     str_out = []
     str_out.append(indent)
     width -= len(indent) 
+    # sep_end is all trailing whitespace in sep
+    # sep_beg is everything before that
+    sep_beg = ''
+    sep_end = sep
+    for i in range(len(sep) - 1, -1, -1):
+        if sep[i] in string.whitespace:
+            continue
+        else:
+            sep_beg = sep[:i + 1]
+            if i == len(sep) - 1:
+                sep_end = ''
+            else:
+                sep_end = sep[i + 1:]
+            break
     for idx, item in enumerate(list): 
         item = str(item) 
         if idx < len(list) - 1 and (not one_per_line): 
-            item += ',' 
+            item += sep_beg
         if one_per_line:
             str_out.append(item)
             if idx < len(list) - 1:
@@ -198,19 +214,23 @@ def pretty_str_list(list, width = 50, indent = '',
             line_counter += 2 
             line_width = 0 
         else: 
-            # + 1 is for the whitespace after the comma.
-            if len(item) + 1 + line_width > width: 
-                str_out.append('\n' + indent) 
+            if line_width == 0:
+                # Because of above if statement, I know that the
+                # len(item) is currently < width
                 str_out.append(item)
-                line_counter += 1 
-                line_width = len(item) 
-            else: 
-                if line_counter == 0 and line_width == 0: 
-                    pass 
-                else: 
-                    item = ' ' + item 
+                line_width += len(item)
+            elif len(sep_end) + len(item) + line_width > width:
+                str_out.append('\n' + indent)
                 str_out.append(item)
-                line_width += len(item) 
+                line_counter += 1
+                line_width = len(item)
+            else:
+                if line_counter == 0 and line_width == 0:
+                    pass
+                else:
+                    item = sep_end + item
+                str_out.append(item)
+                line_width += len(item)
     str_out = ''.join(str_out)
     return(str_out)
 
@@ -262,7 +282,7 @@ def convert_dict_to_dataframe(dict_of_dicts):
     new_df = pd.DataFrame(new_dict, index = index)
     return(new_df)
 
-def get_yes_or_no(input_string):
+def get_yes_or_no(prompt):
     """Get a yes or no response from the user.
 
     Get a yes or no response from the user in a way
@@ -273,17 +293,16 @@ def get_yes_or_no(input_string):
     Code: INPUT_FROM_USER[0].lower()
 
     Args:
-        input_string: String. Prompt fed to input()
+        prompt: String. Prompt fed to input()
     
     Returns:
         Boolean. True if answer was yes. False if no.
 
     Raises: None
     """
-    yn_loop = True
-    while yn_loop:
-        user_input = input(input_string)
-        if user_input is None:
+    while True:
+        user_input = input(prompt)
+        if user_input == '':
             print('ERROR: Please enter yes or no!')
             continue
         if len(user_input) == 0:
@@ -291,14 +310,137 @@ def get_yes_or_no(input_string):
             continue
         user_input = user_input[0].lower()
         if user_input == 'y':
-            yn_loop = False
             return(True)
         elif user_input == 'n':
-            yn_loop = False
             return(False)
         else:
             print('ERROR: Please enter yes or no!')
             continue
+
+def get_command(options, prompt, first_letter=False,
+                case_sensitive=True, error_message=None,
+                empty_str_error_msg=None):
+    """Get a command from the user in a CLI.
+
+    In a command line interactive application, get a command from
+    the user from preset list of options. Provides a stable loop
+    that handles bad input.
+
+    Args:
+        options: List of Strings. Each string is a valid input option
+            to receive from the user. The empty string can be included
+            here as a valid option.
+        prompt: String. The string used as the prompt for input()
+        first_letter: Boolean. If True and all options start with a
+            different letter, valid input can be given as the first
+            letter of any option. To avoid confusing interfaces,
+            'a' and 'A' are not considered different first letters,
+            even if the case_sensitive flag is set.
+        case_sensitive: Boolean. If True, the input does not have
+            to match case to the given options.
+        error_message: String. If not None, this will be used as
+            a generic error message for all invalid input except
+            empty string input. If None, the following will be used:
+                'ERROR: Invalid option!'
+        empty_str_error_msg: String. If not None, this will be used
+            as the error message for invalid empty string input.
+            If None, the following will be used:
+                'ERROR: Empty string is not a valid option!'
+        
+    Returns:
+        A single string matching one of the options passed in
+        options, even if a first_letter abbreviation was
+        given by the user or if input is not case-sensitive.
+
+    Raises:
+        Assertion Error: If first_letter is set True and there are
+            non-unique first letters. This ignores case and the
+            case_sensitive flag.
+    """
+    if error_message is None:
+        error_message = 'ERROR: Invalid option!'
+    if empty_str_error_msg is None:
+        empty_str_error_msg = 'ERROR: Empty string is not a valid option!'
+    if first_letter:
+        first_letters = set()
+        for opt in options:
+            if opt == '':
+                first_letters.add(opt)
+            else:
+                first_letters.add(opt[0].lower())
+        if len(first_letters) != len(options):
+            raise AssertionError('first_letter cannot be True if there are '
+                                 'non-unique first letters in the options! '
+                                 'Case does not count and there cannot be '
+                                 'more than one empty string.')
+        del first_letters
+    opt_set = set(options)
+    if len(opt_set) != len(options):
+        raise AssertionError('options has non-unique entries!')
+    options = opt_set
+    del opt_set
+    if '' in options:
+        empty_string = True
+        options.remove('')
+    else:
+        empty_string = False
+    while True:
+        user_input = input(prompt)
+        # Take care of empty string case to simplify later code
+        if user_input == '':
+            if empty_string:
+                return('')
+            else:
+                print(empty_str_error_msg)
+                continue
+        if user_input in options:
+            return(user_input)
+        elif not case_sensitive:
+            user_input_lower = user_input.lower()
+            for opt in options:
+                opt_lower = opt.lower()
+                if user_input_lower == opt_lower:
+                    return(opt)
+                elif first_letter and len(user_input) == 1:
+                    if user_input_lower == opt_lower[0]:
+                        return(opt)
+            print(error_message)
+            continue
+        elif first_letter and len(user_input) == 1:
+            for opt in options:
+                if user_input == opt[0]:
+                    return(opt)
+            print(error_message)
+            continue
+        else:
+            print(error_message)
+            continue
+
+def pretty_str_text(text, width=80, indent=''):
+    """Returns pretty string of string.
+
+    Given a string, returns a prettified, printable
+    version of the same string. This is a special case
+    of pretty_str_list().
+
+    Args:
+        text: String. The string to be converted to a
+            pretty printable string.
+        width: Integer. The maximum character width of
+            the pretty string.
+        indent: String. A string to prepend to every line
+            of the pretty string.
+    
+    Returns:
+        A printable version of the text string. It conforms
+        to the width restriction and adds the indent to the
+        beginning of each line.
+
+    Raises: None
+    """
+    pretty_str = pretty_str_list(text.split(' '), width=width, indent=indent,
+                                 sep=' ', one_per_line=False)
+    return(pretty_str)
 
 def main():
     pass
